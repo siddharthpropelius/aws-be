9e8f2e2fe48f8ff221b35d9d87dfc0c9c90857d2
siddharth <siddharth17vaishnav@gmail.com>
2024-12-10 22:03:51 +0530

    added endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
+import bodyParser from "body-parser";
+import cors from "cors";
 import express from "express";
+import pg from "pg";
 
 const app = express();
 
-app.get("/", (req: any, res: any) => {
-  return res.status(200).json("hello");
+const pool = new pg.Pool({
+  user: "test_owner",
+  host: "ep-quiet-hill-a1yvcqaq.ap-southeast-1.aws.neon.tech",
+  database: "test",
+  password: "poDTu4ahCPU8",
+  port: 5432,
+  ssl: true,
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+app.get("/users", async (req, res) => {
+  const result = await pool.query("SELECT * FROM users");
+  res.json(result.rows);
+});
+
+app.post("/users", async (req, res) => {
+  const { firstName, lastName, email } = req.body;
+  const result = await pool.query(
+    "INSERT INTO users (first_name, last_name, email) VALUES ($1, $2, $3) RETURNING *",
+    [firstName, lastName, email]
+  );
+  res.json(result.rows[0]);
 });
 
 app.listen(3001, () => {
